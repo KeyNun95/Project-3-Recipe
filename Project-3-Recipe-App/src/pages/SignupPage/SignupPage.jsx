7e7544d55e50aee7e804fc7b17e201f3a54d724f
KeyNun95 <k.nunez2017@gmail.com>
2023-08-02 07:23:08 -0400

    made signup form

--- Project-3-Recipe-App/src/pages/SignupPage/SignupPage.jsx
+++ Project-3-Recipe-App/src/pages/SignupPage/SignupPage.jsx
@@ -1,1 +1,88 @@
+import { useState } from "react"
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import {
+    Button,
+    Form,
+    Grid,
+    Header,
+    Image,
+    Message,
+    Segment,
+    } from "semantic-ui-react";
 
+export default function SignUpPage(){
+    const [state, setState] = useState({
+        username: '',
+        email: '',
+        password: '',
+        passwordConf: ''
+    });
+
+    const [error, setError] = useState('');
+
+    function handleChange(e){
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault();
+    }
+
+    return(
+        <Grid textAlign="center" style={{height: "100vh"}} verticalAlign="middle">
+            <Grid.Column style={{ maxWidth: 450}}>
+                <Header>
+                </Header>
+                <Form autoComplete="off" onSubmit={handleSubmit}>
+                    <Segment stacked>
+                        <Form.Input
+                            name="username"
+                            placeholder="username"
+                            value={state.username}
+                            onChange={handleChange}
+                            required
+                        />
+                        <Form.Input
+                            type="email"
+                            name="email"
+                            placeholder="email"
+                            value={state.email}
+                            onChange={handleChange}
+                            required
+                        />
+                        <Form.Input
+                            name="password"
+                            type="password"
+                            placeholder="password"
+                            value={state.password}
+                            onChange={handleChange}
+                            required
+                        />
+                        <Form.Input
+                            name="passwordConf"
+                            type="password"
+                            placeholder="Confirm password"
+                            value={state.passwordConf}
+                            onChange={handleChange}
+                            required
+                        />
+                        <Form.Field>
+                            <Form.Input
+                                type="file"
+                                name="photo"
+                                placeholder="upload image"
+                            />
+                        </Form.Field>
+                        <Button type="submit" className="btn">
+                            Signup
+                        </Button>
+                    </Segment>
+                    {error ? <ErrorMessage error={error} /> : null}
+                </Form>
+            </Grid.Column>
+        </Grid>
+    );
+}
