9c860515099f0e6fe5fc31841a6b36bfca656890
KeyNun95 <k.nunez2017@gmail.com>
2023-08-03 12:37:07 -0400

    form functions

--- Project-3-Recipe-App/src/components/RecipeForm/RecipeForm.jsx
+++ Project-3-Recipe-App/src/components/RecipeForm/RecipeForm.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import React from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 
 export default function RecipeForm(){
+//STEP 2: set up useState for the form
     const [state, setState] = useState({
         recipeTitle: '',
         ingredientList: '',
         direction: '',
     })
+//STEP 4: since we have a file set up useState for file input
+    const [selectedFile, setSelectedFile] = useState('')
+//STEP 3: write function that handles change in the form
+    function handleChange(e){
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+//STEP 5: write function that handles change in form for file
+    function handleFileInput(e){
+        setSelectedFile(e.target.files[0])
+    }
 
+    function handleSubmit(e){
+    }
+//STEP 1: set up UI
     return(
         <Segment>
             <Form autoComplete="off" onSubmit={handleSubmit}>
